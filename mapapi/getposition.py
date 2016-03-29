b898085471a542f8fd6dee494bb14483b5be75e7
Zhibin Wang <android_zhibinw@163.com>
2016-03-29 16:44:33 +0800

    update.
    Change-Id: I8ada4679eb46eb06158d09debe7c72cde2d636c3

--- mapapi/getposition.py
+++ mapapi/getposition.py
@@ -8,10 +8,20 @@ AK_PRE='&ak='
 ADDR_PRE='&address='
 LOCATION_PRE='&location='
 POSI='&pois=1'
-ADDR='上海浦东新区金科路'
-addr ='上海黄浦区西藏中路268号来'
-add2='翔殷路1003号'
-add3=u'上海市徐汇区天钥桥路580号3楼'
+addrs=[
+u'上海浦东新区金科路',
+u'上海黄浦区西藏中路268号来',
+u'翔殷路1003号',
+u'上海市徐汇区天钥桥路580号3楼',
+u'上海浦东新区浦城路367号',
+u'上海市浦东新区崂山路689号',
+u'上海市济南路9号新天地西楼3楼',
+u'上海市长宁路1018号龙之梦购物中心12楼',
+u'上海市余姚路770-772号2楼',
+u'上海钥桥路318号',
+u'上海市福山路519号',
+u'上海市亮秀路72号'
+]
 def get_position(address):
     lng=None
     lat=None
@@ -24,6 +34,9 @@ def get_position(address):
     lng = json_result['result']['location']['lng']
 
     return success,lng,lat
+def get_positions(addresses):
+    for add in addresses:
+        print add,get_position(add)
 
 def get_address(lng,lat):
     result = rq.get(MAP_URI+AK_PRE+MYAK+POSI+LOCATION_PRE+str(lat)+','+str(lng))
@@ -32,6 +45,6 @@ def get_address(lng,lat):
     return json_result
 
 
-print get_position(add3)
-print get_address(121.44861425015,31.191606250978)
+print get_positions(addrs)
+#print get_address(121.44861425015,31.191606250978)
 
